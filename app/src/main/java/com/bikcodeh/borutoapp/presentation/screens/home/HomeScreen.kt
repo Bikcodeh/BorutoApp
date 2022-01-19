d451ec2d35f394ed35b197abfdee1225b9909a94
Bikcodeh <bikcodeh@gmail.com>
2022-01-18 21:41:45 -0500

    finish creation of rating widget

--- app/src/main/java/com/bikcodeh/borutoapp/presentation/screens/home/HomeScreen.kt
+++ app/src/main/java/com/bikcodeh/borutoapp/presentation/screens/home/HomeScreen.kt
@@ -1,9 +1,13 @@
 package com.bikcodeh.borutoapp.presentation.screens.home
 
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.paging.compose.collectAsLazyPagingItems
+import com.bikcodeh.borutoapp.presentation.components.RatingWidget
+import com.bikcodeh.borutoapp.ui.theme.LARGE_PADDING
 
 @Composable
 fun HomeScreen(
@@ -15,7 +19,7 @@ fun HomeScreen(
     Scaffold(topBar = {
         HomeTopBar(onSearchClicked = {})
     }) {
-
+    RatingWidget(modifier = Modifier.padding(all = LARGE_PADDING), rating = 7.0)
     }
 }
 
