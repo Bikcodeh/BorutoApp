0f091bb0eeb6f8277a70bbe33eba5632cd6887ab
Bikcodeh <bikcodeh@gmail.com>
2022-01-19 19:14:28 -0500

    implements initialize function for remote mediator with a cache time out of 24 hours

--- app/src/main/java/com/bikcodeh/borutoapp/data/paging_source/HeroRemoteMediator.kt
+++ app/src/main/java/com/bikcodeh/borutoapp/data/paging_source/HeroRemoteMediator.kt
@@ -20,6 +20,19 @@ class HeroRemoteMediator @Inject constructor(
     private val heroDao = borutoDatabase.heroDao()
     private val remoteKeysDao = borutoDatabase.heroRemoteKeysDao()
 
+    override suspend fun initialize(): InitializeAction {
+        val currentTime = System.currentTimeMillis()
+        val lastUpdated = remoteKeysDao.getRemoteKeys(heroId = 1)?.lastUpdated ?: 0L
+        val cacheTimeout = 1440
+
+        val diffInMinutes = (currentTime - lastUpdated) / 1000 / 60
+        return if (diffInMinutes.toInt() <= cacheTimeout) {
+            InitializeAction.SKIP_INITIAL_REFRESH
+        } else {
+            InitializeAction.LAUNCH_INITIAL_REFRESH
+        }
+    }
+
     override suspend fun load(loadType: LoadType, state: PagingState<Int, Hero>): MediatorResult {
         return try {
             val page = when (loadType) {
@@ -58,7 +71,8 @@ class HeroRemoteMediator @Inject constructor(
                         HeroRemoteKeys(
                             id = hero.id,
                             prevPage = prevPage,
-                            nextPage = nextPage
+                            nextPage = nextPage,
+                            lastUpdated = response.lastUpdated
                         )
                     }
                     remoteKeysDao.addAllRemoteKeys(keys)
